2bc7bf1199bf4d7678eb7c39870f45269b35c1df
Abdulaziz <abdulazizkomilov2001@gmail.com>
2024-04-01 06:42:28 +0500

    adds

--- core/cdn/conf.py
+++ core/cdn/conf.py
@@ -2,7 +2,7 @@ import os
 
 
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY_ID = os.environ.get('AWS_SECRET_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_S3_SIGNATURE_VIRSION = 's3v4'
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_ENDPOINT_URL = 'https://fra1.digitaloceanspaces.com'
