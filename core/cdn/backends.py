3a3b1336886ca78676159ecdc61aaf3cc3679184
Abdulaziz <abdulazizkomilov2001@gmail.com>
2024-04-01 06:53:58 +0500

    adds

--- core/cdn/backends.py
+++ core/cdn/backends.py
@@ -1,9 +1,9 @@
 from storages.backends.s3boto3 import S3Boto3Storage
 
-class StaticRoot3Boto3Storage(S3Boto3Storage):
+
+class StaticRootS3Boto3Storages(S3Boto3Storage):
     location = 'static'
 
 
-class MediaRoot3Boto3Storage(S3Boto3Storage):
+class MediaRootS3Boto3Storages(S3Boto3Storage):
     location = 'media'
-
